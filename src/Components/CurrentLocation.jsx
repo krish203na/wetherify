f922220364cabaf70664b8390a942afff0d83f59
Krish_Na <krishnapan91@gmail.com>
2024-01-29 11:03:24 +0530

    Current Location Working Done !

--- src/Components/CurrentLocation.jsx
+++ src/Components/CurrentLocation.jsx
@@ -10,9 +10,8 @@ const CurrentLocation = () => {
   });
 
   function getLocation() {
-      if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition( showPosition);
-        //   return `${locationData.latitude},${locationData.longitude}`;
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
     } else {
       console.log("Geolocation is not supported by this browser.");
     }
@@ -24,19 +23,14 @@ const CurrentLocation = () => {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude,
     }));
-    // console.log(`${locationData.latitude}`);
-    // locationData.latitude = position.coords.latitude;
-    // locationData.longitude = position.coords.longitude;
   }
 
-    // getLocation();
-  //   console.log(lat);
+  setTimeout(() => {
+    getLocation();
+  }, 0);
 
   async function currentLocationSubmit(e) {
     e.preventDefault();
-    // getLocation()
-    getLocation()
-    // console.log(getLocation());
     let { current, location } = await searchedLocation(
       `${locationData.latitude},${locationData.longitude}`
     );
@@ -46,15 +40,14 @@ const CurrentLocation = () => {
   }
 
   return (
-    <form className="flex" onSubmit={currentLocationSubmit}>
-      <button
-        className="flex items-center justify-center gap-2 shadow-xl rounded-xl bg-[#9bb0db] px-3 mx-[12px]"
-        type="submit"
-      >
-        <i className="fa-sharp fa-solid fa-location-crosshairs"></i>
-        <span className="hidden md:block">current Location</span>
-      </button>
-    </form>
+    <button
+      onClick={currentLocationSubmit}
+      className="flex items-center justify-center gap-2 shadow-xl rounded-xl bg-[#9bb0db] px-3 mx-[12px]"
+      type="submit"
+    >
+      <i className="fa-sharp fa-solid fa-location-crosshairs"></i>
+      <span className="hidden md:block">current Location</span>
+    </button>
   );
 };
 

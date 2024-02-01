e4142cacee62373b176f51948acf18bb5e6f84b3
Krish_Na <krishnapan91@gmail.com>
2024-02-01 22:40:41 +0530

    Ready to deploy

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,11 +8,11 @@ const Navbar = () => {
     <nav className="w-full md:h-[70px] h-full lg:absolute static text-black z-20 p-[10px] flex justify-between">
       <div className="flex gap-2 justify-center items-center text-2xl">
         <img className="w-[60px] rounded-full" src="./logo.png" alt="" />
-        <h1 className="appName md:block hidden">
+        <h1 className="appName lg:block hidden">
           Wetherif!
         </h1>
       </div>
-      <div className="flex xl:gap-[18vw]">
+      <div className="flex xl:gap-[15vw]">
         <SearchBar />
         <CurrentLocation />
       </div>
